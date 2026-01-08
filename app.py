c0de4a093c1e8ed59a8ef3d2bf1645c020b71eb4
solobro44000-blip <solobro44000@gmail.com>
2026-01-09 00:01:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -593,9 +593,9 @@ async def MaiiiinE():
     global loop, key, iv, region, BOT_UID
 
     # BOT LOGIN UID
-    BOT_UID = int('14060248612')  # <-- FIXED BOT UID
+    BOT_UID = int('12986622624')  # <-- FIXED BOT UID
 
-    Uid, Pw = '4329367556', '388C55606186D83EF4224804D9C9E77E410CFB294737B566FC46643F832D7AFD'
+    Uid, Pw = '4105641482', '3ECF23BB925CB2C06D7F11F3C5FBE4D2BADA93F1B1F52B8C57E0598A835D7FA8'
 
     open_id, access_token = await GeNeRaTeAccEss(Uid, Pw)
     if not open_id or not access_token:
